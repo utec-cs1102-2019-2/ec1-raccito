dc6f593753a70b9948493e24f4c34021d4e7f0c7
unknown <raccito24@gmail.com>
2019-09-11 17:05:29 -0500

    punteros

--- session4a/main.cpp
+++ session4a/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-
+using namespace std;
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    int ival = 42;
+    int *p = &ival;
+    cout << p <<"\t" << *p << "\t" <<&p;
     return 0;
 }
