548d1305725a19d2fb013bf251e0239c9b389359
unknown <raccito24@gmail.com>
2019-09-21 19:51:56 -0500

    ejercicio_2

---
+++ semana_1/semana_A/ejercicio_2/cmake-build-debug/CMakeFiles/ejercicio_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ejercicio_2.dir/main.cpp.obj"
+  "ejercicio_2.pdb"
+  "ejercicio_2.exe"
+  "ejercicio_2.exe.manifest"
+  "libejercicio_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ejercicio_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ semana_1/semana_A/ejercicio_2/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+const float descuento=0.8;
+int main() {
+    float precio=0;
+    int zona;
+    string respuesta,nombrzona;
+    cout<<"Ingrese zona a escojer elija una opcion(#): ";cin>>zona;
+    if(zona==1){
+        precio +=212;
+        nombrzona="Super vip" ;
+    }else if (zona==2){
+        precio +=170;
+        nombrzona="Vip";
+    }else if (zona==3){
+        precio +=136;
+        nombrzona="Preferencial";
+    }else if (zona==4) {
+        precio +=69;
+        nombrzona="General";
+    }else if (zona<1||zona>4){
+        cout<<"intente de nuevo";
+    }
+    cout<<"Usted es usuario claro?: ";cin>>respuesta;
+    if (respuesta =="si"){
+        precio*=descuento;
+    }else if(respuesta == "no"){
+        precio+=0;}
+    cout<<"Usted va para la zona "<<nombrzona<< " debera pagar "<<precio<<endl;
+    return 0;
+}//profe en este ejercicio si demore un poco jaja
